7e81fb69fe076803f3c6656105b714088b25d313
KimScor <58765606+kimjisoo4@users.noreply.github.com>
2024-04-19 20:36:56 +0900

    Add Variable
    [ Added ]
     SerializableReference 용 Variable 추가

---
+++ Extend/Variables/BodyPositionVariable.cs
@@ -0,0 +1,47 @@
+using StudioScor.Utilities;
+using System;
+using UnityEngine;
+
+namespace StudioScor.BodySystem.Variable
+{
+    [Serializable]
+    public class BodyPositionVariable : PositionVariable
+    {
+        [Header(" [ Body Position Variable ] ")]
+        [SerializeField] private BodyTag _bodyTag;
+        [SerializeField] private Vector3 _offset = Vector3.zero;
+
+        private IBodySystem _bodySystem;
+        private BodyPositionVariable _original;
+
+        public override void Setup(GameObject owner)
+        {
+            base.Setup(owner);
+
+            _bodySystem = owner.GetBodySystem();
+        }
+
+        public override IVariable<Vector3> Clone()
+        {
+            var clone = new BodyPositionVariable();
+
+            clone._original = this;
+
+            return clone;
+        }
+
+        public override Vector3 GetValue()
+        {
+            var tag = _original is null ? _bodyTag : _original._bodyTag;
+
+            if (_bodySystem.TryGetBodyPart(tag, out IBodyPart bodypart))
+            {
+                return bodypart.transform.TransformPoint(_original._offset);
+            }
+            else
+            {
+                return _bodySystem.transform.TransformPoint(_original._offset);
+            }
+        }
+    }
+}
---
+++ Extend/Variables/HasBodyConditionVariable.cs
@@ -0,0 +1,39 @@
+﻿using StudioScor.Utilities;
+using System;
+using UnityEngine;
+
+namespace StudioScor.BodySystem.Variable
+{
+    [Serializable]
+    public class HasBodyConditionVariable : ConditionVariable
+    {
+        [Header(" [ Has Body Condition Variable ] ")]
+        [SerializeField] private BodyTag _bodyTag;
+
+        private IBodySystem _bodySystem;
+        private HasBodyConditionVariable _original;
+
+        public override void Setup(GameObject owner)
+        {
+            base.Setup(owner);
+
+            _bodySystem = owner.GetBodySystem();
+        }
+
+        public override IVariable<bool> Clone()
+        {
+            var clone = new HasBodyConditionVariable();
+
+            clone._original = this;
+
+            return clone;
+        }
+
+        public override bool GetValue()
+        {
+            var tag = _original is null ? _bodyTag : _original._bodyTag;
+
+            return _bodySystem.HasBodyPart(tag);
+        }
+    }
+}
